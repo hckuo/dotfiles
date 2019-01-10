bba315d93626632709e681a0ba71d334d5d2e9f6
Hsuan-Chi Kuo <hsuanchikuo@gmail.com>
2019-01-10 15:56:48 -0600

    add tmux

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-update_vimrc() {
+install_vimrc() {
+        echo "Installing vimrc"
         mkdir -p $HOME/.config/nvim;
         cp nvim/init.vim $HOME/.config/nvim;
 }
 
-update_vimrc;
+install_tmux() {
+        echo "Installing tmux.conf"
+        cp tmux/tmux.conf $HOME/.tmux.conf;
+}
+
+install_vimrc;
+install_tmux;
