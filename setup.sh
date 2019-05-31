cd170cad01962afe26f3574a14b592327513800f
Austin <hckuo@ibm.com>
2019-05-31 08:55:02 -0500

    update omz

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 install_vimrc() {
         echo "Installing vimrc"
-        if ! [ -f ~/.vim/autoload/plug.vim ]; then
-            curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        if ! [ -f $HOME/.vim/autoload/plug.vim ]; then
+            curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
         fi
         cp vim/vimrc $HOME/.vimrc;
@@ -14,5 +14,14 @@ install_tmux() {
         cp tmux/tmux.conf $HOME/.tmux.conf;
 }
 
-install_vimrc;
-install_tmux;
+install_oh_my_zsh() {
+        echo "Installing oh-my-zsh"
+        if ! [ -d $HOME/.oh-my-zsh ]; then
+        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+        fi
+        cp zsh/zshrc $HOME/.zshrc
+}
+
+install_vimrc
+install_tmux
+install_oh_my_zsh
