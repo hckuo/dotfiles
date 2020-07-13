322ac49e0e32890a80501a4ddd6b6f1d6eccf08f
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2020-07-13 18:46:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ install_oh_my_zsh() {
         if ! [ -d $HOME/.oh-my-zsh ]; then
         sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
         fi
+        rm $HOME/.zshrc
         ln -s $BASEDIR/zsh/zshrc $HOME/.zshrc
 }
 
